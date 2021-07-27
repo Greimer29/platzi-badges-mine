22f0f360ccf8484bc33bcf3ae5f50ccb5d2fac66
Greimer29 <greimerabelguzman29@gmail.com>
2021-07-27 12:39:20 -0400

    Añadiendole los estilos al componente

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import configLogo from '../images/badge-header.svg';
 
+import './styles/Badge.css';
+
 class Badge extends React.Component{
     render(){
         return(
-            <div>
-                <div>
+            <div className="badge">
+                <div className="badge__header">
                     <img src={configLogo} alt="Esta es una imagen hijo de perra" />
                 </div>
-                <div>
-                    <img src="" alt="Otra imagen hijo de perra" />
-                    <h1>Greimer <br />Kmón</h1>
+                <div className="badge__section-name">
+                    <img className="badge__avatar" src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?d=identicon" alt="Otra imagen hijo de perra" />
+                    <h1>Un pana <br />Ahí</h1>
                 </div>
-                <div>
-                    <p>Description</p>
-                    <p>More description</p>
+                <div className="badge__section-info">
+                    <p>Frontend Engineer</p>
+                    <p>@sparragus</p>
                 </div>
-                <div>
+                <div className="badge__footer">
                     #PlatziConfigMmgvo
                 </div>
             </div>
