42db5385326e98f3025d62c0caebba0fda3baa5b
Greimer29 <greimerabelguzman29@gmail.com>
2021-08-01 20:20:53 -0400

    Implomentando eventos sencillos

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
+    handleChange = e =>{
+        console.log({
+            name: e.target.name,
+            value: e.target.value
+        })
+    }
+    handleClick = e =>{
+        console.log("Botón clickeado");
+    }
+    handleSubmit = e =>{
+        e.preventDefault();
+        console.log('Formulario enviado')
+    }
+
     render(){
         return(
             <div>
                 <h1>New Atendant</h1>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label>Firts Name</label>
-                        <input className="form-control" name="firtsName" type="text" />
+                        <input onChange={this.handleChange} className="form-control" name="firtsName" type="text" />
                     </div>
-                    <button className="btn btn-primary">
+                    <button onClick={this.handleClick} type="button" className="btn btn-primary">
                         Save
                     </button>
                 </form>
