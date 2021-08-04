ba0b5f5cbfabfa5c6676bc22d4d47cb975689dbf
Greimer29 <greimerabelguzman29@gmail.com>
2021-08-03 21:50:37 -0400

    Desarrollando a medias el componente (BadgeList)

--- src/components/BadgeList.js
+++ src/components/BadgeList.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import './styles/BadgeList.css';
 
 class BadgeList extends React.Component {
     render() {
         return (
             <ul className="list-unstyled">
-            {this.props.badges.map(badge => {
-                return(
-                    <li key={badge.id}>
-                        <p>
-                            {badge.firstName}  {badge.lastName}
-                        </p>
-                    </li>
-                )
-            })}
-        </ul>
+                {this.props.badges.map(badge => {
+                    return(
+                        <li key={badge.id} className="list__container">
+                            <div className="image__container">
+                                <img className="avatar__image" src={badge.avatarUrl} alt="Imagen" />
+                            </div>
+                            <div className="container">
+                                <div>{badge.firstName} {badge.lastName}</div>
+                                <div> {badge.twitter} </div>
+                                <div>{badge.jobTitle}</div>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
         )
     }
 }
