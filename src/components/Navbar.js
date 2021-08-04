275e23073f4f24585fc2f49d25faf3d27f9f9ee5
Greimer29 <greimerabelguzman29@gmail.com>
2021-08-04 13:21:44 -0400

    Cambiando (<a> por <Link>)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from '../images/logo.svg';
 import './styles/Navbar.css';
+import {Link} from 'react-router-dom';
+
 class Navbar extends React.Component {
     render(){
         return(
             <div className="navbar">
                 <div className="container-fluid">
-                    <a className="navbar__brand" href="/">
+                    <Link className="navbar__brand" to="/">
                         <img className="navbar__brand-logo" src={logo} alt="Brand_logo" />
                         <span className="font-weight-ligth">Platzi</span>
                         <span className="font-weight-bold">Config</span>
-                    </a>
+                    </Link>
                 </div>
             </div>
         )
